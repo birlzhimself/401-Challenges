9886caacfa8f429071a0662adaf92d36737ed4ce
Hugo Rebelo <122793759+birlzhimself@users.noreply.github.com>
2023-06-22 23:23:29 +0100

    Update Challenge26.py

--- Challenge26.py
+++ Challenge26.py
@@ -4,34 +4,28 @@
 # Purpose        Proceeds to ping 8.8.8.8 indefinitely while printing a timestamp and success or failure message to the console and writing it to a log file with a maximum size.
 # Why            Learn how to use the logging library.
 
-import subprocess
+import os
 import time
 import logging
 
 # Configure logging settings
 logging.basicConfig(filename="ping_log.log", level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
+# Define the IP address to ping
 ip_address = "8.8.8.8"
 
-logging.info('Starting script... Pinging 8.8.8.8\n')
-
-def ping(ip_address):
-    try:
-        result = subprocess.run(["ping", "-c", "1", ip_address], capture_output=True, text=True, timeout=5)
-        if result.returncode == 0:
-            status = "success"
-        else:
-            status = "failure"
-
-        log_entry = f"Network {status} to {ip_address}"
-        logging.info(log_entry)
-
-    except subprocess.TimeoutExpired:
-        logging.error(f"Timeout error occurred while pinging {ip_address}")
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-
-# Main loop
+# Start the ping loop
 while True:
-    ping(ip_address)
-    time.sleep(2)
+    # Send a ping to the IP address
+    response = os.system("ping -c 1 " + ip_address)
+    # Check the response code
+    if response == 0:
+        status = "Success"
+    else:
+        status = "Failure"
+    # Print the timestamp and status message to the console
+    print(time.strftime("%Y-%m-%d %H:%M:%S"), status)
+    # Write the timestamp and status message to the log file
+    logging.info(time.strftime("%Y-%m-%d %H:%M:%S"), status)
+    # Sleep for 1 second
+    time.sleep(1)
