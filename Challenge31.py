3d654b84ef7b2aa58ff6c129c8d697d233001425
Hugo Rebelo <122793759+birlzhimself@users.noreply.github.com>
2023-06-29 18:29:45 +0100

    Update and rename Challenge30.py to Challenge31.py

---
+++ Challenge31.py
@@ -0,0 +1,21 @@
+import os
+import sys
+# Get the file name to search for.
+filename = input("Enter the file name to search for: ")
+# Get the directory to search in.
+directory = input("Enter the directory to search in: ")
+# Check if the directory exists.
+if not os.path.exists(directory):
+    print("The directory does not exist.")
+    sys.exit(1)
+# Get a list of all the files in the directory.
+files = os.listdir(directory)
+# Search each file in the directory by name.
+for file in files:
+    # Check if the file name matches the file name to search for.
+    if file == filename:
+        # Print the file name and location.
+        print(os.path.join(directory, file))
+# Print how many files were searched and how many hits were found.
+print("Number of files searched:", len(files))
+print("Number of hits found:", len([file for file in files if file == filename]))
